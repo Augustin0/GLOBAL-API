9b115e09f732bd4e2cdaf1306022148c0042e55a
Skenshee Augustin <s.augustin@kolectiv.tech>
2021-11-02 12:52:05 -0400

    Api 3

--- src/includes/config_db.php
+++ src/includes/config_db.php
@@ -9,7 +9,7 @@ class DBconfig
    { 
       $user =getenv()["DB_USER"];//getenv("DB_USER");
       $password = getenv()["DB_PASS"];//getenv("DB_PASS");
-      $db_host = getenv()["DB_HOST"].":".getenv()["DB_PORT"] ;//getenv("DB_HOST");
+      $db_host = getenv()["DB_HOST"] ;//getenv("DB_HOST");
       $db_name =getenv()["DB_NAME"];// getenv("DB_NAME");
       $this->dsn = "pgsql:host=$db_host;dbname=$db_name;";
       if (!$this->connection) $this->connection = new PDO($this->dsn,$user,$password);
