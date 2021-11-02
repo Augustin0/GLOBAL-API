90a4ea33244ccda28dfbe80864b062a1a79596c5
Skenshee Augustin <s.augustin@kolectiv.tech>
2021-11-02 09:40:55 -0400

    Api heroku 3

--- src/core/initialize.php
+++ src/core/initialize.php
@@ -9,14 +9,14 @@ else defined("ROOT_DIR") ? NULL : define("ROOT_DIR",/* DS . "xampp" . DS . "htdo
 defined("COR") ? NULL : define("COR", ROOT_DIR . "src" . DS . "core" . DS);
 defined("MODELS") ? NULL : define("MODELS", ROOT_DIR . "src" . DS . "models" . DS);
 defined("PACK") ? NULL : define("PACK", ROOT_DIR . "vendor" . DS);*/
+//print(dirname(__FILE__));
 
-
-include_once("../../vendor/autoload.php");
-//include_once(COR . "initialize.php");
-include_once("../includes/config_db.php");
-include_once("../includes/config_db.php");
-include_once("../includes/fetch_api_config.php");
-include_once("../models/users/utils_user.php");
-include_once("../models/users/user.php");
-include_once("../models/utils_article.php");
-include_once("../models/new.php");
+include_once(dirname(__DIR__)."/../vendor/autoload.php");
+include_once(dirname(__FILE__) . "/initialize.php");
+include_once(dirname(__DIR__)."/includes/config_db.php");
+include_once(dirname(__DIR__)."/includes/config_headers.php");
+include_once(dirname(__DIR__)."/includes/fetch_api_config.php");
+include_once(dirname(__DIR__)."/models/users/utils_user.php");
+include_once(dirname(__DIR__)."/models/users/user.php");
+include_once(dirname(__DIR__)."/models/utils_article.php");
+include_once(dirname(__DIR__)."/models/new.php");
