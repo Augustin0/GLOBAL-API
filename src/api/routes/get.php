57177d0269b5a598c61b3e872d32ad804ae75210
Skenshee Augustin <s.augustin@kolectiv.tech>
2021-11-02 17:07:53 -0400

    API 3

--- src/api/routes/get.php
+++ src/api/routes/get.php
@@ -11,15 +11,13 @@ $method                                                                         
 if ($method != "GET")                                                                die("Mehod $method  not allowed");
 
 $article                                                                           = new Article($connection, $API_KEY);
-$validation           = (isset($_GET["q"]) && isset($_GET["to"]) && isset($_GET["lang"]) && isset($_GET["ctr"])) ? true :false;
+$validation           = (isset($_GET["q"]) && isset($_GET["lang"]) && isset($_GET["ctr"])) ? true :false;
 
 if (!$validation)                                                                             die("Parameters missing 1");
-$validation   = (strlen($_GET["q"]) < 1 | strlen($_GET["to"]) < 1 | strlen($_GET["lang"]) < 1 | strlen($_GET["ctr"]) < 1) ? false : true;
+$validation   = (strlen($_GET["q"]) < 1 | strlen($_GET["lang"]) < 1 | strlen($_GET["ctr"]) < 1) ? false : true;
 if (!$validation)                                                                            die("Parameters missing 2");
 
 $article->q                                                                  = htmlspecialchars(strip_tags($_GET["q"]));
-$article->at                                                                = htmlspecialchars(strip_tags($_GET["at"]));
-$article->to                                                                = htmlspecialchars(strip_tags($_GET["to"]));
 $article->lang                                                           = htmlspecialchars(strip_tags($_GET["lang"]));
 $article->ctr                                                            = htmlspecialchars(strip_tags($_GET["ctr"]));
 $res                                                                                                  = $article->get();
--- src/includes/config_db.php
+++ src/includes/config_db.php
@@ -7,13 +7,13 @@ class DBconfig
    public $connection;
    public function __construct()
    { 
-      $user =getenv()["DB_USER"];//getenv("DB_USER");
-      $password = getenv()["DB_PASS"];//getenv("DB_PASS");
-      $db_host = getenv()["DB_HOST"] ;//getenv("DB_HOST");
-      $db_name =getenv()["DB_NAME"];// getenv("DB_NAME");
+      $user =getenv()["DB_USER"];
+      $password = getenv()["DB_PASS"];
+      $db_host = getenv()["DB_HOST"] ;
+      $db_name =getenv()["DB_NAME"];
       $dbtype=getenv()["DB_TYPE"];
       $this->dsn = "$dbtype:host=$db_host;dbname=$db_name;";
-      if (!$this->connection) $this->connection = new PDO($this->dsn,$user,$password);
+      if (!$this->connection) $this->connection = new PDO($this->dsn,$user,"");
    }
 
    public function get()
