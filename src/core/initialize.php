2ba6377e8885fe42398d396d8be16366f5ec06e5
Skenshee Augustin <s.augustin@kolectiv.tech>
2021-11-02 09:23:52 -0400

    Api heroku 3

--- src/core/initialize.php
+++ src/core/initialize.php
@@ -1,22 +1,22 @@
 <?php
 //local :C:\xampp\htdocs\APIS\what_new_api\core
 //REMOTE :
-defined("DS") ? NULL : define("DS", DIRECTORY_SEPARATOR);
+/*defined("DS") ? NULL : define("DS", DIRECTORY_SEPARATOR);
 
 if(getenv()["Local"]=="false") define("ROOT_DIR","https://news-api-global.herokuapp.com".DS);
-else defined("ROOT_DIR") ? NULL : define("ROOT_DIR",/* DS . "xampp" . DS . "htdocs" . DS . "APIS" . DS . "what_new_api" */"https://news-api-global.herokuapp.com". DS);
-defined("INC") ? NULL : define("INC", ROOT_DIR . "src" . DS . "includes" . DS);
+else defined("ROOT_DIR") ? NULL : define("ROOT_DIR",/* DS . "xampp" . DS . "htdocs" . DS . "APIS" . DS . "what_new_api" *///"https://news-api-global.herokuapp.com". DS);
+/*defined("INC") ? NULL : define("INC", ROOT_DIR . "src" . DS . "includes" . DS);
 defined("COR") ? NULL : define("COR", ROOT_DIR . "src" . DS . "core" . DS);
 defined("MODELS") ? NULL : define("MODELS", ROOT_DIR . "src" . DS . "models" . DS);
-defined("PACK") ? NULL : define("PACK", ROOT_DIR . "vendor" . DS);
+defined("PACK") ? NULL : define("PACK", ROOT_DIR . "vendor" . DS);*/
 
 
-include_once(PACK . "autoload.php");
-include_once(COR . "initialize.php");
-include_once(INC . "config_headers.php");
-include_once(INC . "config_db.php");
-include_once(INC . "fetch_api_config.php");
-include_once(MODELS . DS . "users" . DS . "utils_user.php");
-include_once(MODELS . DS . "users" . DS . "user.php");
-include_once(MODELS . "utils_article.php");
-include_once(MODELS . "new.php");
+include_once("../../vendor/autoload.php");
+//include_once(COR . "initialize.php");
+include_once("../includes/config_db.php");
+include_once("../includes/config_db.php");
+include_once("../includes/fetch_api_config.php");
+include_once("../models/users/utils_user.php");
+include_once("../models/users/user.php");
+include_once("../models/utils_article.php");
+include_once("../models/new.php");
